8a13e00251284a1d442102870936622ea5b29b57
mike-stumpf <mike.a.stumpf@gmail.com>
2015-05-30 09:44:43 -0400

    started on init functionality

--- libraries/chorus/_chorus.js
+++ libraries/chorus/_chorus.js
@@ -3,3 +3,17 @@ _chorus.version = _chorus.version || {
     version: 0.2,
     lastUpdated: new Date().toDateString()
 };
+_chorus.init = _chorus.init || function(ele){
+    if (ele === undefined){
+        return false;
+    }
+    else if (document.getElementById(ele) !== null){
+        console.log("id");
+    }
+    else if (document.getElementsByClassName(ele).length > 0){
+        console.log("class");
+    }
+    else {
+        return false;
+    }
+};
