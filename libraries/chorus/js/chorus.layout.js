0042f0757fb2a412dcac99f91377cf10b8360bb7
mike-stumpf <mike.a.stumpf@gmail.com>
2015-06-13 22:25:00 -0400

    modified instrument parameter

--- libraries/chorus/js/chorus.layout.js
+++ libraries/chorus/js/chorus.layout.js
@@ -1,53 +1,64 @@
 _chorus.layout = _chorus.layout || {
     init : function(element){
         var instrument = _chorus.config.instrument;
-        var content;
+        if (typeof instrument === "string") {
+            element.innerHTML = _chorus.layout.selectInstrument(instrument);
+        }
+        else if (Array.isArray(instrument)){
+            var content = "";
+            for(var i = 0; i < instrument.length; i++){
+                content += _chorus.layout.selectInstrument(instrument[i]);
+            }
+            element.innerHTML = content;
+        }
+        else {
+            _chorus.events.messages.sendMessage("instrument paramter invalid");
+        }
+    },
+    selectInstrument: function(instrument){
+        var content = "";
         var key;
-        switch(instrument){
+        switch (instrument) {
             case "all":
-                content = "";
                 for (key in _chorus.data.instruments.main) {
-                    if (_chorus.data.instruments.main.hasOwnProperty(key)){
+                    if (_chorus.data.instruments.main.hasOwnProperty(key)) {
                         content += _chorus.layout.html.instrument(key, _chorus.data.instruments.main[key]);
                     }
                 }
                 for (key in _chorus.data.instruments.other) {
-                    if (_chorus.data.instruments.other.hasOwnProperty(key)){
+                    if (_chorus.data.instruments.other.hasOwnProperty(key)) {
                         content += _chorus.layout.html.instrument(key, _chorus.data.instruments.other[key]);
                     }
                 }
-                element.innerHTML = content;
+
                 break;
             case "main":
-                content = "";
                 for (key in _chorus.data.instruments.main) {
-                    if (_chorus.data.instruments.main.hasOwnProperty(key)){
+                    if (_chorus.data.instruments.main.hasOwnProperty(key)) {
                         content += _chorus.layout.html.instrument(key, _chorus.data.instruments.main[key]);
                     }
                 }
-                element.innerHTML = content;
                 break;
             case "alternate":
-                content = "";
                 for (key in _chorus.data.instruments.alternateGuitar) {
-                    if (_chorus.data.instruments.alternateGuitar.hasOwnProperty(key)){
+                    if (_chorus.data.instruments.alternateGuitar.hasOwnProperty(key)) {
                         content += _chorus.layout.html.instrument(key, _chorus.data.instruments.alternateGuitar[key]);
                     }
                 }
-                element.innerHTML = content;
                 break;
             default :
-                if (_chorus.data.instruments.main.hasOwnProperty(instrument)){
-                    element.innerHTML = _chorus.layout.html.instrument(instrument, _chorus.data.instruments.main[instrument]);
+                if (_chorus.data.instruments.main.hasOwnProperty(instrument)) {
+                    content = _chorus.layout.html.instrument(instrument, _chorus.data.instruments.main[instrument]);
                 }
-                else if(_chorus.data.instruments.other.hasOwnProperty(instrument)){
-                    element.innerHTML = _chorus.layout.html.instrument(instrument, _chorus.data.instruments.other[instrument]);
+                else if (_chorus.data.instruments.other.hasOwnProperty(instrument)) {
+                    content = _chorus.layout.html.instrument(instrument, _chorus.data.instruments.other[instrument]);
                 }
-                else if(_chorus.data.instruments.alternateGuitar.hasOwnProperty(instrument)){
-                    element.innerHTML = _chorus.layout.html.instrument(instrument, _chorus.data.instruments.alternateGuitar[instrument]);
+                else if (_chorus.data.instruments.alternateGuitar.hasOwnProperty(instrument)) {
+                    content = _chorus.layout.html.instrument(instrument, _chorus.data.instruments.alternateGuitar[instrument]);
                 }
                 break;
         }
+        return content;
     }
 };
 
