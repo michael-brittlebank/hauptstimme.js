9a8fe1f6fc465c78aab7c19cd1bb605db307e5b6
mike-stumpf <mike.a.stumpf@gmail.com>
2016-01-01 21:22:23 -0600

    data - refactored scale logic

--- src/js/modules/scales/chorus.logic.scales.js
+++ src/js/modules/scales/chorus.logic.scales.js
@@ -1,68 +1,75 @@
 _chorus.logic.scales = _chorus.logic.scales || {
-    /**
-     *
-     */
-    compile: {
-        init: function() {
-            var root, tones, tone, letters, scale, scaleKey, scaleName, scaleArray;
-            var output = {
-                main:{},
-                other: {}
-            };
-            for (var i = 0; i < _chorus.data.notes.count.tones; i++) {
-                root = _chorus.logic.notes.getNoteByToneDefault(i);
-                for (scale in _chorus.data.scales){
-                    if (_chorus.data.scales.hasOwnProperty(scale) && scale !== "searchable" && scale !== "count") {
-                        for (scaleKey in _chorus.data.scales[scale]) {
-                            if (_chorus.data.scales[scale].hasOwnProperty(scaleKey)) {
-                                scaleArray = _chorus.data.scales[scale][scaleKey];
-                                tones = [i];
-                                for (var j = 0; j < scaleArray.length - 1; j++) {
-                                    tone = (tones[j] + scaleArray[j]) % 12;
-                                    tones.push(tone);
-                                }
-                                letters = _chorus.logic.scales.compile.getLetters(root, tones);
-                                if (letters !== false) {
-                                    scaleName = letters[0] + " " + _chorus.logic.helpers.capitalize(scaleKey.replace(/_/g, " "));
-                                    output[scale][scaleName] = {
-                                        tones: tones,
-                                        letters: letters
-                                    };
+        /**
+         *
+         */
+        compile: {
+            init: function() {
+                var root,
+                    tones,
+                    tone,
+                    letters,
+                    scale,
+                    scaleKey,
+                    scaleName,
+                    scaleArray,
+                    output = {
+                        main:{},
+                        other:{}
+                    };
+                for (var i = 0; i < _chorus.data.notes.count.tones; i++) {
+                    root = _chorus.logic.notes.getNoteByToneDefault(i);
+                    for (scale in _chorus.data.scales){
+                        if (_chorus.data.scales.hasOwnProperty(scale) && scale !== 'searchable' && scale !== 'count') {
+                            for (scaleKey in _chorus.data.scales[scale]) {
+                                if (_chorus.data.scales[scale].hasOwnProperty(scaleKey)) {
+                                    scaleArray = _chorus.data.scales[scale][scaleKey];
+                                    tones = [i];
+                                    for (var j = 0; j < scaleArray.length - 1; j++) {
+                                        tone = (tones[j] + scaleArray[j]) % 12;
+                                        tones.push(tone);
+                                    }
+                                    letters = _chorus.logic.scales.compile.getLetters(root, tones);
+                                    if (letters !== false) {
+                                        scaleName = letters[0] + ' ' + _chorus.logic.helpers.capitalize(scaleKey.replace(/_/g, ' '));
+                                        output[scale][scaleName] = {
+                                            tones: tones,
+                                            letters: letters
+                                        };
+                                    }
                                 }
                             }
                         }
                     }
                 }
-            }
-            return JSON.stringify(output);
-        },
-        getLetters: function(root, tones){
-            var letters = [root];
-            var letterProgression = _chorus.logic.notes.getLetterProgression(root);
-            for (var k = 1; k < tones.length; k++) {
-                if (tones.length == 7) {
-                    var letter = _chorus.logic.notes.getNoteByToneForce(tones[k], letterProgression[k]);
-                    if (letter !== false){
-                        letters.push(letter);
-                    }
-                    else {
-                        if (root.indexOf("#") >= 0){
-                            letters = _chorus.logic.scales.compile.getLetters(letterProgression[1]+"b",tones);
-                            break;
+                return JSON.stringify(output);
+            },
+            getLetters: function(root, tones){
+                var letters = [root],
+                    letterProgression = _chorus.logic.notes.getLetterProgression(root);
+                for (var k = 1; k < tones.length; k++) {
+                    if (tones.length == 7) {
+                        var letter = _chorus.logic.notes.getNoteByToneForce(tones[k], letterProgression[k]);
+                        if (letter !== false){
+                            letters.push(letter);
                         }
                         else {
-                            return false;
+                            if (root.indexOf('#') >= 0){
+                                letters = _chorus.logic.scales.compile.getLetters(letterProgression[1]+'b',tones);
+                                break;
+                            }
+                            else {
+                                return false;
+                            }
                         }
                     }
+                    else {
+                        letters.push(_chorus.logic.notes.getNoteByToneDefault(tones[k]));
+                    }
                 }
-                else {
-                    letters.push(_chorus.logic.notes.getNoteByToneDefault(tones[k]));
-                }
+                return letters;
             }
-            return letters;
         }
-    }
-};
+    };
 
 /**
  * main search function for searching scales
@@ -86,13 +93,13 @@ _chorus.logic.scales.searchScales = _chorus.searchScales = function(tones, scale
             }
             else if (!_chorus.logic.helpers.validator.isValidScaleSearchMode(scaleSearchMode)) {
                 scalesToSearch = _chorus.defaultConfig.scaleSearchMode;
-                _chorus.events.messages.sendMessage(_chorus.data.dictionary.error_value + "invalid scale search mode");
+                _chorus.events.messages.sendMessage(_chorus.data.dictionary.error_value + 'invalid scale search mode');
             }
             else {
                 scalesToSearch = scaleSearchMode;
             }
             //search scales
-            if (scalesToSearch === "main" || scalesToSearch === "all") {
+            if (scalesToSearch === 'main' || scalesToSearch === 'all') {
                 for (scaleKey in _chorus.data.scales.searchable.main) {
                     if (_chorus.data.scales.searchable.main.hasOwnProperty(scaleKey)) {
                         if (_chorus.logic.scales.scaleContains(_chorus.data.scales.searchable.main[scaleKey], tones)) {
@@ -101,7 +108,7 @@ _chorus.logic.scales.searchScales = _chorus.searchScales = function(tones, scale
                     }
                 }
             }
-            if (scalesToSearch === "other" || scalesToSearch === "all") {
+            if (scalesToSearch === 'other' || scalesToSearch === 'all') {
                 for (scaleKey in _chorus.data.scales.searchable.other) {
                     if (_chorus.data.scales.searchable.other.hasOwnProperty(scaleKey)) {
                         if (_chorus.logic.scales.scaleContains(_chorus.data.scales.searchable.other[scaleKey], tones)) {
@@ -112,11 +119,11 @@ _chorus.logic.scales.searchScales = _chorus.searchScales = function(tones, scale
             }
         }
         else {
-            _chorus.events.messages.sendMessage(_chorus.data.dictionary.error_notFound + "no selected notes found");
+            _chorus.events.messages.sendMessage(_chorus.data.dictionary.error_notFound + 'no selected notes found');
         }
         _chorus.searchResult.scales = data;
-        _chorus.events.dispatchEvent("chorusScaleSearchComplete", "chorusJS has finished searching scales");
-        if (callback && typeof callback !== "string") {
+        _chorus.events.dispatchEvent('chorusScaleSearchComplete', 'chorusJS has finished searching scales');
+        if (callback && typeof callback !== 'string') {
             callback(data);
         }
     }
