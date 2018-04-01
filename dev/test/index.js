71dd1b7ba15af98bb113bab9ebe287fa5ad2235a
mike-stumpf <mike.a.stumpf@gmail.com>
2018-04-01 18:55:11 +0100

    code-conversion - bug fix for chai tests

--- dev/test/index.js
+++ dev/test/index.js
@@ -1,12 +1,11 @@
 'use strict';
 
-import { HauptstimmeJs } from '../src/hauptstimme';
-
 const expect = require('chai').expect;
+const HauptstimmeJs = require('../../dist/src/hauptstimme').HauptstimmeJs;
 
 describe('#HauptstimmeJs.getAvailableInstruments', function() {
     it('should return an array of instruments', function() {
         const result = HauptstimmeJs.getAvailableInstruments();
-        expect(result).to.be.greaterThan(0);
+        expect(result).to.be.an('array');
     });
 });
--- dist/src/index.component.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class Hauptstimme {
-    /**
-     * Adds commas to a number
-     * @param {number} number
-     * @param {string} locale
-     * @return {string}
-     */
-    static getLocalString(number: number, locale?: string): string;
-}
