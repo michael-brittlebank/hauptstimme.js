ab8c890108df1dbbbc9fc2ddc84606b2262f30e6
Mike Stumpf <mstumpf@amfam.com>
2021-04-22 13:55:56 -0500

    ported hauptstimme for easier higher level access

--- src/exports.js
+++ src/exports.js
@@ -1,4 +1,9 @@
 const Constants = require('./constants')
-const Instruments = require('./instruments')
+const Instruments = require('./data/instruments.data')
+const Scales = require('./data/scales.data')
+const Chords = require('./data/chords.data')
+const Search = require('./services/search.service')
+const Util = require('./services/util.service')
+const Hauptstimme = require('./hauptstimme')
 
-module.exports = { Constants, Instruments }
+module.exports = { Constants, Instruments, Scales, Chords, Search, Util, Hauptstimme}
