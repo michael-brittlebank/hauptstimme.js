3b989e5eed448c3ef6e01953185fe8b06b80a66c
Mike Stumpf <mstumpf@amfam.com>
2021-04-22 14:16:01 -0500

    fixed failing tests

--- test/services/util.service.test.js
+++ test/services/util.service.test.js
@@ -7,54 +7,54 @@ const {noteConstant, letterConstant, flatEntity, sharpEntity} = require('../../s
 describe('./src/services/util.service', () => {
  describe('#getFormattedLetterFromNote', () => {
   it('gets correct sharp/flat when AB - no note array', () => {
-   expect(getFormattedLetterFromNote(noteConstant.AB)).toEqual(`${letterConstant.B}${flatEntity}`);
+   expect(getFormattedLetterFromNote({note: noteConstant.AB})).toEqual(`${letterConstant.B}${flatEntity}`);
   });
 
   it('gets correct sharp/flat when AB - when array has A', () => {
-   expect(getFormattedLetterFromNote(noteConstant.AB, [noteConstant.A])).toEqual(`${letterConstant.B}${flatEntity}`);
+   expect(getFormattedLetterFromNote({note: noteConstant.AB, noteArray:[noteConstant.A]})).toEqual(`${letterConstant.B}${flatEntity}`);
   });
 
   it('gets correct sharp/flat when AB - when array has B', () => {
-   expect(getFormattedLetterFromNote(noteConstant.AB, [noteConstant.B])).toEqual(`${letterConstant.A}${sharpEntity}`);
+   expect(getFormattedLetterFromNote({note:noteConstant.AB, noteArray:[noteConstant.B]})).toEqual(`${letterConstant.A}${sharpEntity}`);
   });
  })
 
  describe('#getLetterFromNote', () => {
   it('translates numerical value into letter key', () => {
-   expect(getLetterFromNote(noteConstant.A)).toEqual(letterConstant.A);
+   expect(getLetterFromNote({note:noteConstant.A})).toEqual(letterConstant.A);
   });
 
   it('translates numerical value into letter key', () => {
-   expect(getLetterFromNote(noteConstant.DE)).toEqual(letterConstant.DE);
+   expect(getLetterFromNote({note:noteConstant.DE})).toEqual(letterConstant.DE);
   });
  })
 
  describe('#modulo', () => {
   it('correctly modulos over remainder', () => {
-   expect(modulo(12, 3)).toEqual(0);
+   expect(modulo({integer:12, modulus:3})).toEqual(0);
   });
 
   it('correctly modulos over remainder', () => {
-   expect(modulo(12, 5)).toEqual(2);
+   expect(modulo({integer:12,modulus: 5})).toEqual(2);
   });
 
   it('correctly modulos over remainder', () => {
-   expect(modulo(12, 7)).toEqual(5);
+   expect(modulo({integer:12,modulus: 7})).toEqual(5);
   });
  })
 
  describe('#adjustNoteBySteps', () => {
   it('adds whole step to note', () => {
-   expect(adjustNoteBySteps(noteConstant.AB, 2)).toEqual(noteConstant.C);
+   expect(adjustNoteBySteps({note:noteConstant.AB, halfSteps:2})).toEqual(noteConstant.C);
   });
   it('adds half step to note and modulos over the end of the note array', () => {
-   expect(adjustNoteBySteps(noteConstant.GA, 1)).toEqual(noteConstant.A);
+   expect(adjustNoteBySteps({note:noteConstant.GA, halfSteps:1})).toEqual(noteConstant.A);
   });
   it('subtracts whole step from note', () => {
-   expect(adjustNoteBySteps(noteConstant.D, -2)).toEqual(noteConstant.C);
+   expect(adjustNoteBySteps({note:noteConstant.D, halfSteps:-2})).toEqual(noteConstant.C);
   });
   it('subtracts half step from note and modulos over the end of the note array', () => {
-   expect(adjustNoteBySteps(noteConstant.A, -1)).toEqual(noteConstant.GA);
+   expect(adjustNoteBySteps({note:noteConstant.A,halfSteps: -1})).toEqual(noteConstant.GA);
   });
  })
 });
