ac87e545fe35e98a4f932eab5b5a041d95adbf97
Mike Stumpf <mstumpf@amfam.com>
2021-04-22 13:57:18 -0500

    removed v3

--- dev/exports/interfaces.exports.ts
+++
@@ -1,6 +0,0 @@
-export * from '../interfaces/chord.interface';
-export * from '../interfaces/chordOrScalePrimitiveInterface';
-export * from '../interfaces/instrument.interface';
-export * from '../interfaces/scale.interface';
-export * from '../interfaces/searchRequest.interface';
-export * from '../interfaces/searchResponse.interface';
--- dev/interfaces/searchRequest.interface.ts
+++
@@ -1,9 +0,0 @@
-import { NoteConstant } from '../constants/note.constant';
-
-/**
- * Interface for search requests
- */
-export interface SearchRequestInterface {
-    rootNote?: NoteConstant;
-    notes: NoteConstant[];
-}
